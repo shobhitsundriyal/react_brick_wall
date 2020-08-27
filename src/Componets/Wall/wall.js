e7f24f8f677e73279bd4cd97690b2f4f4bce5099
unknown <shobhitsundriyal0170@gmail.com>
2020-08-28 00:12:51 +0530

    Components is Componets ;-)

---
+++ src/Componets/Wall/wall.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Wall extends React.Component{
+
+    //@override render function
+    render = () => {
+        return (
+            //Some html
+            <p>Hello World</p>
+        )
+    }
+
+}
+
+export default Wall; //To use it in another files
